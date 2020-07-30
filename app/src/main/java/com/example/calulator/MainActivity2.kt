74566a8a87d44141d8beea98fa2433e0f1070f5c
ali-moghtader <ali.devil1999@gmail.com>
2020-07-30 16:34:23 +0430

    added some features and fixed some bugs

--- app/src/main/java/com/example/calulator/MainActivity2.kt
+++ app/src/main/java/com/example/calulator/MainActivity2.kt
@@ -6,6 +6,10 @@ import android.text.TextUtils.isEmpty
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main2.*
+import java.lang.Math.*
+import kotlin.math.cos
+import kotlin.math.tan
+import kotlin.math.tanh
 
 class MainActivity2 : AppCompatActivity() {
     var operator_var:String=""
@@ -15,6 +19,7 @@ class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        number_view.text=cos(0.0).toString()
     }
     fun onClick(view: View){
         when(view.id){
@@ -33,6 +38,10 @@ class MainActivity2 : AppCompatActivity() {
             R.id.btn_divide->operate("/")
             R.id.btn_minus->operate("-")
             R.id.btn_multiply->operate("*")
+            R.id.btn_deposit->operate("%")
+            R.id.btn_sin->operate("sin")
+            R.id.btn_cos->operate("cos")
+            R.id.btn_tan->operate("tan")
             R.id.btn_equal->calculate()
         }
     }
@@ -46,16 +55,28 @@ class MainActivity2 : AppCompatActivity() {
         number_view.text=""
     }
     fun operate(op:String){
+        if ((op=="sin"||op=="cos"||op=="tan")&&first_num.isNotEmpty()&&second_num.isEmpty()){
+            operator_var=op
+
+
+            calculate()
+        }
+        else
         if(operator_var.isEmpty()||(first_num.isNotEmpty() &&second_num.isEmpty() )){
             operator_var=op
             first_num=number_view.text.toString()
             number_view.text=""
         }
         else {
-            first_num=calculate()
-            operator_var=op
-            number_view.text=""
-
+            first_num=calculate()//0
+            operator_var=op//cos
+            if ((op=="sin"||op=="cos"||op=="tan")&&first_num.isNotEmpty()&&second_num.isEmpty()){
+                calculate()
+                operator_var=""
+            }
+            else {
+                number_view.text = ""
+            }
         }
     }
     fun calculate():String{
@@ -69,6 +90,10 @@ class MainActivity2 : AppCompatActivity() {
                 "-" -> res = first_num.toDouble() - second_num.toDouble()
                 "*" -> res = first_num.toDouble() * second_num.toDouble()
                 "/" -> res = first_num.toDouble() / second_num.toDouble()
+                "%" -> res = first_num.toDouble() % second_num.toDouble()
+                "sin"->res= kotlin.math.sin(first_num.toDouble()*0.0174532925)
+                "cos"->res= kotlin.math.cos(first_num.toDouble()*0.0174532925)
+                "tan"->res= kotlin.math.tan(first_num.toDouble()*0.0174532925)
                 "="-> number_view.text=first_num
 
 
